48c18d0c0be7f5c668ad570d7673b013645b71ee
Adaobiokeke <adaobiotiji@yahoo.com>
2021-06-24 11:15:13 +0100

    new products

--- src/Bags/bagsdata.js
+++ src/Bags/bagsdata.js
@@ -6,8 +6,120 @@ import bag5 from '../Assets/bag7.jpeg'
 import bag6 from '../Assets/bag8.jpeg'
 import bag7 from '../Assets/bag9.jpeg'
 import bag8 from '../Assets/bag10.jpeg'
+import  newbag1 from '../Assets/newbag1.jpeg'
+import newbag2  from "../Assets/newbag2.jpeg"
+import newbag3  from "../Assets/newbag3.jpeg"
+import newbag4  from "../Assets/newbag4.jpeg"
+import newbag5  from "../Assets/newbag5.jpeg"
+import newbag6  from "../Assets/newbag6.jpeg"
+import newbag7  from "../Assets/newbag7.jpeg"
+import newbag8  from "../Assets/newbag8.jpeg"
+import newbag9  from "../Assets/newbag9.jpeg"
+import newbag10  from "../Assets/newbag10.jpeg"
+import newbag11  from "../Assets/newbag11.jpeg"
+import newbag12  from "../Assets/newbag12.jpeg"
+import newbag13  from "../Assets/newbag13.jpeg"
+import newbag14  from "../Assets/newbag14.jpeg"
+import newbag15  from "../Assets/newbag15.jpeg"
+import newbag16  from "../Assets/newbag16.jpeg"
 
 export const Bgdatas = [
+    {
+        title:'Details ',
+        image:newbag13,
+        Price:'9,000.00',
+        content:'mini designer Turkey box handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag14,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag15,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag16,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag1,
+        Price:'N50,000.00',
+        content:'River Island designer Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag2,
+        Price:'N57,000.00',
+        content:'Brown designer Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag3,
+        Price:'N45,000.00',
+        content:'Blue designer italian Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag4,
+        Price:'N20,500.00',
+        content:'Red designer Purse',
+    },
+    {
+        title:'Details ',
+        image:newbag5,
+        Price:'N42,500.00',
+        content:'Nine West designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag6,
+        Price:'N34,500.00',
+        content:' designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag7,
+        Price:'N34,500.00',
+        content:' designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag8,
+        Price:'N34,500.00',
+        content:'White designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag9,
+        Price:'N46,000.00',
+        content:'Valentino designer leather purse',
+    },
+    {
+        title:'Details ',
+        image:newbag10,
+        Price:'N46,000.00',
+        content:'Valentino designer leather purse',
+    },
+    {
+        title:'Details ',
+        image:newbag11,
+        Price:'N29,000.00',
+        content:'white designer clutch purse',
+    },
+    {
+        title:'Details ',
+        image:newbag12,
+        Price:'N65,000.00',
+        content:'Karl Lagerfeld designer handbag',
+    },
     {
         title:'Details ',
         image:Bag1,
--- src/ModalContainers/DicContainerdata.js
+++ src/ModalContainers/DicContainerdata.js
@@ -27,8 +27,23 @@ import  necklace6 from '../Assets/necklace6.jpeg'
 import  necklace7 from '../Assets/necklace7.jpeg'
 import  necklace8 from '../Assets/necklace8.jpeg'
 import  necklace9 from '../Assets/necklace9.jpeg'
-// import  Perf from '../Assets/Perfumes.png'
-// import  Perf from '../Assets/Perfumes.png'
+import  newbag1 from '../Assets/newbag1.jpeg'
+import newbag2  from "../Assets/newbag2.jpeg"
+import newbag3  from "../Assets/newbag3.jpeg"
+import newbag4  from "../Assets/newbag4.jpeg"
+import newbag5  from "../Assets/newbag5.jpeg"
+import newbag6  from "../Assets/newbag6.jpeg"
+import newbag7  from "../Assets/newbag7.jpeg"
+import newbag8  from "../Assets/newbag8.jpeg"
+import newbag9  from "../Assets/newbag9.jpeg"
+import newbag10  from "../Assets/newbag10.jpeg"
+import newbag11  from "../Assets/newbag11.jpeg"
+import newbag12  from "../Assets/newbag12.jpeg"
+import newbag13  from "../Assets/newbag13.jpeg"
+import newbag14  from "../Assets/newbag14.jpeg"
+import newbag15  from "../Assets/newbag15.jpeg"
+import newbag16  from "../Assets/newbag16.jpeg"
+
 export const Divdatas = [
     {
         title:'Details',
@@ -36,6 +51,102 @@ export const Divdatas = [
         Price:'N1,000.00',
         size:'3ml',
         content:"A vibrant, floral oil fragrance where the top notes of Sparkling Lemon and Lime are enhanced by the fruity nuances of Peach. The floral heart is a radiant blend of Pink Rose, Magnolia and Frangipani. The dark seductive base of Sensuous Musk, Amber and Precious Woods brings this fragrance to life."
+    },
+    {
+        title:'Details ',
+        image:newbag13,
+        Price:'9,000.00',
+        content:'mini designer Turkey box handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag14,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag15,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag16,
+        Price:'9,000.00',
+        content:'mini designer Turkey handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag1,
+        Price:'N50,000.00',
+        content:'River Island designer Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag2,
+        Price:'N57,000.00',
+        content:'Brown designer Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag3,
+        Price:'N45,000.00',
+        content:'Blue designer italian Leather hand bag',
+    },
+    {
+        title:'Details ',
+        image:newbag4,
+        Price:'N20,500.00',
+        content:'Red designer Purse',
+    },
+    {
+        title:'Details ',
+        image:newbag5,
+        Price:'N42,500.00',
+        content:'Nine West designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag6,
+        Price:'N34,500.00',
+        content:' designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag7,
+        Price:'N34,500.00',
+        content:' designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag8,
+        Price:'N34,500.00',
+        content:'White designer leather handbag',
+    },
+    {
+        title:'Details ',
+        image:newbag9,
+        Price:'N46,000.00',
+        content:'Valentino designer leather purse',
+    },
+    {
+        title:'Details ',
+        image:newbag10,
+        Price:'N46,000.00',
+        content:'Valentino designer leather purse',
+    },
+    {
+        title:'Details ',
+        image:newbag11,
+        Price:'N29,000.00',
+        content:'white designer clutch purse',
+    },
+    {
+        title:'Details ',
+        image:newbag12,
+        Price:'N65,000.00',
+        content:'Karl Lagerfeld designer handbag',
     },
     {
         title:'Details ',
